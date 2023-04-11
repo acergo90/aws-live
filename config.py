d429fefd70d11bdbe12cb4fbfe613d9f24858977
acergo90 <35990032+acergo90@users.noreply.github.com>
2023-04-11 15:13:29 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cr5ihmsbvvh2.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "hojunhui-employee"
+customregion = "us-east-1"
